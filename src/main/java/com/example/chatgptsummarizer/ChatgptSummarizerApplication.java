4a9b89aae63d9c73de42b7714bd358734a2eb5e1
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-09-19 08:54:17 +0200

    Setup project

---
+++ src/main/java/com/example/chatgptsummarizer/ChatgptSummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatgptsummarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptSummarizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgptSummarizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chatgptsummarizer/ChatgptSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatgptsummarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptSummarizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
